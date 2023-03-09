b6c59e5c7b94508294107f1dc5640acc9c94aab2
Miller <gelaseen@gmail.com>
2023-03-09 19:16:35 +0700

    Created UITabBarController Added main UITabBarController's screens

---
+++ Pawpet/Modules/MainTabBarController.swift
@@ -0,0 +1,43 @@
+//
+//  MainTabBarController.swift
+//  Pawpet
+//
+//  Created by Robert Miller on 05.02.2023.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let vc1 = UINavigationController(rootViewController: SearchViewController())
+        let vc2 = UINavigationController(rootViewController: PostViewController())
+        let vc3 = UINavigationController(rootViewController: MessagerViewController())
+        let vc4 = UINavigationController(rootViewController: ProfileViewController())
+
+        viewControllers = [vc1, vc2, vc3, vc4]
+
+        setTitles()
+        setImages()
+
+        let appearance = UITabBarItem.appearance()
+        let attributes = [NSAttributedString.Key.font:UIFont(name: "ChalkboardSE-Regular", size: 12)]
+        appearance.setTitleTextAttributes(attributes as [NSAttributedString.Key : Any], for: .normal)
+    }
+
+    func setTitles() {
+        tabBar.items?[0].title = "Search"
+        tabBar.items?[1].title = "Post"
+        tabBar.items?[2].title = "Messages"
+        tabBar.items?[3].title = "Profile"
+    }
+
+    func setImages() {
+        tabBar.items?[0].image = UIImage(systemName: "magnifyingglass")
+        tabBar.items?[1].image = UIImage(systemName: "plus.circle")
+        tabBar.items?[2].image = UIImage(systemName: "message")
+        tabBar.items?[3].image = UIImage(systemName: "person")
+    }
+}
---
+++ Pawpet/UIColor + Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor + Extension.swift
+//  Pawpet
+//
+//  Created by Robert Miller on 09.03.2023.
+//
+
+import Foundation
